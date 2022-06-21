0d7265da5f75d4278ac50781f945874e118b9c35
muztaba <hasanat.muztaba@gmail.com>
2022-06-21 15:41:44 -0500

    Phase 1 courses

---
+++ lab.one.phase.one/src/main/java/com/waa/lab/one/phase/one/entiry/Course.java
@@ -0,0 +1,20 @@
+package com.waa.lab.one.phase.one.entiry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Course {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+}
