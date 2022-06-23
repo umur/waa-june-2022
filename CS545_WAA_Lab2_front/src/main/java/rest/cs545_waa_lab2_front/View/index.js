dc61d532879790480070f05babae3ee5fc210892
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-06-23 14:18:25 -0500

    added

--- CS545_WAA_Lab2_front/src/main/java/rest/cs545_waa_lab2_front/View/index.js
+++ CS545_WAA_Lab2_front/src/main/java/rest/cs545_waa_lab2_front/View/index.js
@@ -1,4 +1,23 @@
 
-window.onload=function (){
-    let  course=  document.getElementById()
+const  coursetable =  document.getElementById("ctn");
+const btn = document.getElementById("btn")
+window.onload= course;
+function course(){
+    fetch("https://localhost:2030/courses")
+        .then(function (res){
+            return res.json();
+        })
+        .then(function (course){
+            course.forEach(function (course){
+                const div =document.createElement('div');
+                div.innerHTML=course;
+                coursetable.appendChild(div);
+            })
+            alert("get successfully");
+            //console.log(course);
+        })
 }
+btn.addEventListener('click', function(){
+    coursetable.innerHTML='';
+course();
+})
